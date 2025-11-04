51ca9a65f37ffd45625e68c504552f7c951b6758
yildizkonaklari <ordulusel@gmail.com>
2025-11-04 22:32:08 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 );
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);

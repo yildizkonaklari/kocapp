34eeecdee71967221de5833aa8f064773aa44d3b
yildizkonaklari <ordulusel@gmail.com>
2025-11-04 22:21:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,11 @@ export default function App() {
     </div>
   );
 }
+import StudentDetail from "./pages/StudentDetail";
+
+<Routes>
+  <Route path="/" element={<Dashboard />} />
+  <Route path="/students" element={<Students />} />
+  <Route path="/students/:id" element={<StudentDetail />} />
+  <Route path="*" element={<Navigate to="/" />} />
+</Routes>

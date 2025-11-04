f506767e324cf312851561a03dd4dea596f7157e
yildizkonaklari <ordulusel@gmail.com>
2025-11-04 18:04:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
 
+export default function App() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col bg-gray-100 min-h-screen">
+        <Navbar />
+        <Dashboard />
+      </div>
+    </div>
+  );
+}

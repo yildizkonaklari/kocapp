63be0ec7308cc8bd630c1cafb7e292f0dbd2e1a2
yildizkonaklari <ordulusel@gmail.com>
2025-11-04 22:14:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "./firebase";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Students from "./pages/Students";
 import Login from "./pages/Login";
 
 export default function App() {
@@ -13,9 +15,7 @@ export default function App() {
     setUser(currentUser);
   });
 
-  const handleLogout = () => {
-    signOut(auth);
-  };
+  const handleLogout = () => signOut(auth);
 
   if (!user) return <Login onLogin={() => setUser(true)} />;
 
@@ -24,7 +24,11 @@ export default function App() {
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gray-100 min-h-screen">
         <Navbar onLogout={handleLogout} />
-        <Dashboard />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </div>
     </div>
   );

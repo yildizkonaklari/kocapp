ca92b55aaed44d769ee1286879aa3a22811fb55d
yildizkonaklari <ordulusel@gmail.com>
2025-11-04 21:31:41 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
-content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-theme: {
-  extend: {},
-},
-plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
